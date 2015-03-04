a8bb5478b967ee8f1380494233d91aba904d6bde
saasbook <saasbook@saasbook.(none)>
2015-03-03 23:58:55 -0500

    4.8

--- Documents/myrottenpotatoes/app/controllers/movies_controller.rb
+++ Documents/myrottenpotatoes/app/controllers/movies_controller.rb
@@ -12,15 +12,33 @@ class MoviesController < ApplicationController
 	
 # in app/controllers/movies_controller.rb
      
-    def show
-      id = params[:id] # retrieve movie ID from URI route
-      @movie = Movie.find(id) # look up movie by unique ID
-      # will render app/views/movies/show.html.haml by default
-    end
+  def show
+    id = params[:id] # retrieve movie ID from URI route
+    @movie = Movie.find(id) # look up movie by unique ID
+     # will render app/views/movies/show.html.haml by default
+  end
 
-   def new
-     # default: render 'new' template
-   end
+  def new
+    # default: render 'new' template
+  end
+
+  def edit
+    @movie = Movie.find params[:id]
+  end
+   
+  def update
+    @movie = Movie.find params[:id]
+    @movie.update_attributes!(params[:movie])
+    flash[:notice] = "#{@movie.title} was successfully updated."
+    redirect_to movie_path(@movie)
+  end 
+
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    flash[:notice] = "Movie '#{@movie.title}' deleted."
+    redirect_to movies_path
+  end
 
 end
 
